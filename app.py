11e15759cd590d0f5fe27ea6999664df9eb657ee
Sinitsa Uladzimir <uladzimirsinitsa@gmail.com>
2022-09-06 17:07:45 +0300

    v0.2

--- app.py
+++ app.py
@@ -1,34 +1,44 @@
 
-from fastapi import FastAPI
+import os
 import sqlite3
+from datetime import datetime
+
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
 
-FILE_DB = '/data/test_db.db'
+DB = os.environ['FILE_DB']
 
-db = sqlite3.connect(FILE_DB)
-
-headlines = [
-    "url",
-    "data",
-    "status"
-]
+db = sqlite3.connect(DB)
 
 create_query = "create table if not exists url_status_data_db (\
-    url, data, status)"
+    url, status)"
 c = db.cursor()
 c.execute(create_query)
 db.commit()
 
 
-def create_record(url, data, status):
+class Record(BaseModel):
+    url: str
+    status: int
+
+
+def create_record(url, status):
     c.execute(f"INSERT INTO url_status_data_db VALUES(\
-        {url}, {data}, {status})")
+        {url}, {status})")
     db.commit()
 
 
-@app.get("/")
-async def root(url, data, status):
-    create_record(url, data, status)
+@app.post("/")
+async def root(record: Record):
+    dict_ = jsonable_encoder(record)
+    url = dict_.get("url")
+    status = dict_.get("status")
+    create_record(url, status)
     return {"msg": "record create"}
