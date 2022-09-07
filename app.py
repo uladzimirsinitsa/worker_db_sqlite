1a5653d5855860df88337dcdc0ecd1368e7752e4
Sinitsa Uladzimir <uladzimirsinitsa@gmail.com>
2022-09-07 15:29:51 +0300

    v0.3

--- app.py
+++ app.py
@@ -10,35 +10,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = FastAPI()
-
-
 DB = os.environ['FILE_DB']
+
+
+class Record_(BaseModel):
+    url: str
+    status_: int
+
+
+app = FastAPI()
 
 db = sqlite3.connect(DB)
 
-create_query = "create table if not exists url_status_data_db (\
-    url, status)"
+create_table = "CREATE TABLE IF NOT EXISTS url_status_db(\
+        urls STRING PRIMARY KEY, status INT)"
 c = db.cursor()
-c.execute(create_query)
-db.commit()
+c.execute(create_table)
 
 
-class Record(BaseModel):
-    url: str
-    status: int
-
-
-def create_record(url, status):
-    c.execute(f"INSERT INTO url_status_data_db VALUES(\
-        {url}, {status})")
+def create_record(url, status_):
+    parameters = (url, status_)
+    query = "INSERT OR IGNORE INTO url_status_db VALUES (?, ?)"
+    c.execute(query, parameters)
     db.commit()
 
 
 @app.post("/")
-async def root(record: Record):
-    dict_ = jsonable_encoder(record)
-    url = dict_.get("url")
-    status = dict_.get("status")
-    create_record(url, status)
-    return {"msg": "record create"}
+async def root(record: Record_):
+    data = jsonable_encoder(record)
+    url = data.get("url")
+    status_ = data.get("status_")
+    create_record(url, status_)
+    return {"status": "OK"}
